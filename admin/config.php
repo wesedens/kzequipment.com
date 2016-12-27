9697d7fd91932edd33d91886aa553f6da99c6909
Wes Edens <wes.k.edens@gmail.com>
2016-12-27 16:58:31 -0700

    installation: move config files

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.kzequipment.com/admin/');
+define('HTTP_CATALOG', 'http://www.kzequipment.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.kzequipment.com/admin/');
+define('HTTPS_CATALOG', 'http://www.kzequipment.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home6/kzequipm/public_html/admin/');
+define('DIR_SYSTEM', '/home6/kzequipm/public_html/system/');
+define('DIR_IMAGE', '/home6/kzequipm/public_html/image/');
+define('DIR_LANGUAGE', '/home6/kzequipm/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home6/kzequipm/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home6/kzequipm/public_html/system/config/');
+define('DIR_CACHE', '/home6/kzequipm/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home6/kzequipm/public_html/system/storage/download/');
+define('DIR_LOGS', '/home6/kzequipm/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home6/kzequipm/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home6/kzequipm/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home6/kzequipm/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kzequipm_cube1da');
+define('DB_PASSWORD', 'uLPrt_tkL38k');
+define('DB_DATABASE', 'kzequipm_cubecart1da');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.kzequipment.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.kzequipment.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home6/kzequipm/public_html/catalog/');
+define('DIR_SYSTEM', '/home6/kzequipm/public_html/system/');
+define('DIR_IMAGE', '/home6/kzequipm/public_html/image/');
+define('DIR_LANGUAGE', '/home6/kzequipm/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home6/kzequipm/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home6/kzequipm/public_html/system/config/');
+define('DIR_CACHE', '/home6/kzequipm/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home6/kzequipm/public_html/system/storage/download/');
+define('DIR_LOGS', '/home6/kzequipm/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home6/kzequipm/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home6/kzequipm/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kzequipm_cube1da');
+define('DB_PASSWORD', 'uLPrt_tkL38k');
+define('DB_DATABASE', 'kzequipm_cubecart1da');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
